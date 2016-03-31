d1b151f331d4fb693c6ac373b2f7f21f7bec9c03
Danny Willems <contact@danny-willems.be>
2016-03-31 21:45:11 +0200

    use enum to sum type

--- screen_orientation.mli
+++ screen_orientation.mli
@@ -1,39 +1,25 @@
 (* --------------------------------------------------------------------------- *)
-class screen_orientation : Ojs.t ->
-  object
-    inherit Ojs.obj
-    method lock_orientation     : string -> unit
-    method orientation          : string
-    method unlock_orientation   : unit
-  end
-(* --------------------------------------------------------------------------- *)
+type orientation_type =
+  | Portrait_primary [@js "portrait-primary"]
+  | Portrait_secondary [@js "portrait-secondary"]
+  | Landscape_primary [@js "landscape-primary"]
+  | Landscape_secondary [@js "landscape-secondary"]
+  | Portrait [@js "portrait"]
+  | Landscape [@js "landscape"]
+  [@@js.enum]
 
-(* --------------------------------------------------------------------------- *)
 [@@@js.stop]
-type orientation
+val portrait                  : orientation_type
+val portrait_primary          : orientation_type
+val portrait_secondary        : orientation_type
+val landscape                 : orientation_type
+val landscape_primary         : orientation_type
+val landscape_secondary       : orientation_type
 
-val portrait                  : orientation
-val portrait_primary          : orientation
-val portrait_secondary        : orientation
-val landscape                 : orientation
-val landscape_primary         : orientation
-val landscape_secondary       : orientation
-
-val str_to_orientation        : string -> orientation
-
-val orientation_to_js_string  : orientation -> string
+val orientation_to_string     : orientation_type -> string
 [@@@js.start]
 
 [@@@js.implem
-type orientation =
-  | Portrait_primary
-  | Portrait_secondary
-  | Landscape_primary
-  | Landscape_secondary
-  | Portrait
-  | Landscape
-  | Unknown
-
 let portrait              = Portrait
 let portrait_primary      = Portrait_primary
 let portrait_secondary    = Portrait_secondary
@@ -41,24 +27,24 @@ let landscape             = Landscape
 let landscape_primary     = Landscape_primary
 let landscape_secondary   = Landscape_secondary
 
-let str_to_orientation str = match str with
-  | "portrait"            -> Portrait
-  | "portrait-primary"    -> Portrait_primary
-  | "portrait-secondary"  -> Portrait_secondary
-  | "landscape"           -> Landscape
-  | "landscape-primary"   -> Landscape_primary
-  | "landscape-secondary" -> Landscape_secondary
-  | _                     -> Unknown
-
-let orientation_to_js_string o = match o with
+let orientation_to_string o = match o with
   | Portrait_primary        -> "portrait-primary"
   | Portrait_secondary      -> "portrait-secondary"
   | Landscape_primary       -> "landscape-primary"
   | Landscape_secondary     -> "landscape-secondary"
   | Portrait                -> "portrait"
   | Landscape               -> "landscape"
-  | _                       -> ""
 ]
+(* --------------------------------------------------------------------------- *)
+
+(* --------------------------------------------------------------------------- *)
+class screen_orientation : Ojs.t ->
+  object
+    inherit Ojs.obj
+    method lock_orientation     : orientation_type -> unit
+    method orientation          : string
+    method unlock_orientation   : unit
+  end
 (* --------------------------------------------------------------------------- *)
 
 (* --------------------------------------------------------------------------- *)
