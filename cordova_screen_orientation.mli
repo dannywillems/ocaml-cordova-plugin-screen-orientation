54668063892655ed0a298d03e437c77803d5689c
Danny Willems <contact@danny-willems.be>
2016-06-07 15:56:35 +0200

    Remove object and use functions with js.global

--- cordova_screen_orientation.mli
+++ cordova_screen_orientation.mli
@@ -9,45 +9,26 @@ type orientation_type =
   [@@js.enum]
 
 [@@@js.stop]
-val portrait                  : orientation_type
-val portrait_primary          : orientation_type
-val portrait_secondary        : orientation_type
-val landscape                 : orientation_type
-val landscape_primary         : orientation_type
-val landscape_secondary       : orientation_type
-
-val orientation_to_string     : orientation_type -> string
+val orientation_type_to_str : orientation_type -> string
 [@@@js.start]
-
 [@@@js.implem
-let portrait              = Portrait
-let portrait_primary      = Portrait_primary
-let portrait_secondary    = Portrait_secondary
-let landscape             = Landscape
-let landscape_primary     = Landscape_primary
-let landscape_secondary   = Landscape_secondary
-
-let orientation_to_string o = match o with
-  | Portrait_primary        -> "portrait-primary"
-  | Portrait_secondary      -> "portrait-secondary"
-  | Landscape_primary       -> "landscape-primary"
-  | Landscape_secondary     -> "landscape-secondary"
-  | Portrait                -> "portrait"
-  | Landscape               -> "landscape"
+let orientation_type_to_str c = match c with
+  | Portrait_primary -> "portrait-primary"
+  | Portrait_secondary -> "portrait-secondary"
+  | Landscape_primary -> "landscape-primary"
+  | Landscape_secondary -> "landscape-secondary"
+  | Portrait -> "portrait"
+  | Landscape -> "landscape"
 ]
 (* --------------------------------------------------------------------------- *)
 
 (* --------------------------------------------------------------------------- *)
-class screen_orientation : Ojs.t ->
-  object
-    inherit Ojs.obj
-    method lock_orientation     : orientation_type -> unit
-    method orientation          : string
-    method unlock_orientation   : unit
-  end
-(* --------------------------------------------------------------------------- *)
+val lock_orientation     : orientation_type -> unit
+[@@js.global "screen.lockOrientation"]
 
-(* --------------------------------------------------------------------------- *)
-val t : unit -> screen_orientation
-[@@js.get "screen"]
+val orientation          : orientation_type
+[@@js.global "screen.orientation"]
+
+val unlock_orientation   : unit -> unit
+[@@js.global "screen.unlockOrientation"]
 (* --------------------------------------------------------------------------- *)
